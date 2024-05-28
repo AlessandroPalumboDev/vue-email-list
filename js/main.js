f9ad04ff2d94a98e2216507e6d3bc3056212eec2
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-28 17:23:39 +0200

    [UPD] Creazione array con indirizzi diversi

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
-console.log('prova');
-
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-
+    data() {
+        return {
+            emails: [],
+            url: 'https://flynn.boolean.careers/exercises/api/random/mail',
         };
     },
 
+    methods: {
+        apiCall() {
+            // ciclo per pushare in array 10 elementi richiamando ogni volta l' api per avere 10 elementi diversi
+            for (let i = 0; i < 10; i++) {
+                axios.get(this.url).then((response) => {
+                    this.emails.push(response.data.response);
+                });
+            }
+        },
+    },
 
+    created() {
+        // richiamo il metodo alla creazione della pagina
+        this.apiCall();
+    },
 }).mount('#app')
--- readme.md
+++ readme.md
@@ -6,6 +6,3 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 - Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 NB: axios è la via
 https://axios-http.com/docs/intro
-
-## Svolgimento:
-
